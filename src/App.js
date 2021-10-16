01940b31d3a0ca2d9e758074e0124fd2404f339b
barsik2 <arslan.bovaeff@gmail.com>
2021-10-16 18:53:57 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div>
+      <header>Блок Header </header>
+      <p>azaza </p>
+      <span>hello</span> <span>hello</span>
+    </div>
+  );
+}
